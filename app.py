471c56dfca8e753fce57c18c67cc89faf63bdce3
shriyansmachabatula <shriyans2004@gmail.com>
2024-10-29 20:02:57 -0400

    fixing more

--- app.py
+++ app.py
@@ -29,14 +29,6 @@ board = {
     31: "black", 32: "red", 33: "black", 34: "red", 35: "black", 36: "red"
 }
 
-
-
-@app.route("/")
-def home():
-    return "Welcome to the Roulette Game API!"
-
-
-
 @app.route("/spin/<bet>/<colorChoice>")
 def spin_once(bet, colorChoice):
     global balance
